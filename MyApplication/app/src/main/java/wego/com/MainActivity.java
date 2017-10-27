596e8ab001bdd35c47e349653658f96bd0e7e1fe
石长洪 <shichanghong@hongaojs.com>
2017-10-27 09:13:20 +0800

    添加jar包

--- MyApplication/app/src/main/java/wego/com/MainActivity.java
+++ MyApplication/app/src/main/java/wego/com/MainActivity.java
@@ -3,11 +3,18 @@ package wego.com;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
---
+++ MyApplication/app/src/main/java/wego/com/httpApi/BaiduApi.java
@@ -0,0 +1,9 @@
+package wego.com.httpApi;
+
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+
+public interface BaiduApi{
+
+}
